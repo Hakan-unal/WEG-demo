a9440740c0e718763b97db52c517cef9c9053869
Con Sınov <hakanunal@devtagon.com>
2022-06-18 00:48:23 +0300

    updated

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { gql } from "@apollo/client";
 import client from "../apollo-client";
-import { Button, Table } from "antd"
+import { Button, Table, Image, Space, Popover } from "antd"
+import moment from 'moment';
+import { AiFillEye } from "react-icons/ai";
+
+
+const popoverContent = (
+  <div>
+    Personelin detaylı görünümü için tıklayınız
+  </div>
+);
 
 export default function Home(props) {
 
@@ -12,40 +20,63 @@ export default function Home(props) {
 
   const columns = [
     {
-      title: 'Name',
-      dataIndex: 'mission_name',
-      key: 'mission_name',
+      title: 'Dahili No',
+      dataIndex: 'index',
+      key: 'index',
     },
     {
-      title: 'Age',
-      dataIndex: 'age',
-      key: 'age',
+      title: 'Görsel',
+      key: 'image',
+      dataIndex: 'image',
+      render: (image) => (
+        <Image alt='Resim yüklenmemiş' width={200} preview={false} src={image}></Image>
+      ),
+    },
+    {
+      title: 'Ad Soyad',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: 'Şirkete Giriş Tarihi',
+      dataIndex: 'date',
+      key: 'date',
     },
     {
       title: 'Address',
       dataIndex: 'address',
       key: 'address',
     },
+    {
+      title: 'Action',
+      key: 'action',
+      sorter: true,
+      render: () => (
+        <Space size="middle">
+          <Popover content={popoverContent}>
+            <Button size='large' icon={<AiFillEye size={30} />} />
+          </Popover>
+        </Space>
+      )
+    },
+
   ];
 
-  const dataSource = [
-    {
-      key: '1',
-      name: 'Mike',
-      age: 32,
-      address: '10 Downing Street',
-    },
-    {
-      key: '2',
-      name: 'John',
-      age: 42,
-      address: '10 Downing Street',
-    },
-  ];
+
 
   useEffect(() => {
-    console.log(props.data.launchesPast)
-    setTableData(props.data.launchesPast)
+    const tempArr = props.data.launchesPast.map((data, index) => {
+      return {
+        key: index,
+        index: data.id,
+        name: data.mission_name,
+        date: moment(data.launch_date_local).format("DD/MM/YYYY"),
+        image: data.ships.length !== 0 ? data.ships[0].image : null,
+        address: data.rocket.rocket_name
+      }
+    })
+
+    setTableData(tempArr)
   }, [])
 
   return (
@@ -70,7 +101,7 @@ export async function getStaticProps() {
   const { data } = await client.query({
     query: gql`
       query GetLaunches {
-        launchesPast(limit: 20){
+        launchesPast(limit: 200){
         id
         mission_name
         launch_date_local
